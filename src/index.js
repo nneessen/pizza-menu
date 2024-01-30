a4707f4ca3c0e4ff759b435ce5d79a59b58477e1
Nick Neessen <nick.neessen@gmail.com>
2024-01-30 17:24:10 -0600

    completed lesson 47. conditional rendering using map method for arrays

--- src/index.js
+++ src/index.js
@@ -69,32 +69,26 @@ function Menu() {
   return (
     <main className='menu'>
       <h2>Our Menu</h2>
-      <Pizza 
-        name='Pizza Spinaci' 
-        ingredients='Tomato, mozarella, spinach, and ricotta cheese' 
-        photoName='pizzas/spinaci.jpg' 
-        price={10}
-      />
-      <Pizza 
-        name="Pizza Funghi"
-        ingredients="tomato, mushrooms"
-        price={12}
-        photoName='pizzas/funghi.jpg'
-      />
+
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name}/>
+        ))}
+      </ul>
     </main>
   );
 };
 
 function Pizza(props) {
     return (
-      <div className="pizza">
-        <img src={props.photoName} alt={props.name} />
+      <li className="pizza">
+        <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
         <div>
-            <h3>{props.name}</h3>
-            <p>{props.ingredients}</p>
-            <span>{props.price}</span>
+            <h3>{props.pizzaObj.name}</h3>
+            <p>{props.pizzaObj.ingredients}</p>
+            <span>{props.pizzaObj.price}</span>
         </div>
-      </div>
+      </li>
     );
   };
 
