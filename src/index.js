262d6b1b9c1c69aa98a23df90afcd250e711a074
Nick Neessen <nick.neessen@gmail.com>
2024-01-31 17:39:07 -0600

    refactored all props args as to not have to use props for every attr

--- src/index.js
+++ src/index.js
@@ -83,17 +83,17 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
 
-  if(props.pizzaObj.soldOut) return null;
+  if(pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -108,13 +108,19 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online!</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} openHour={openHour}/>
       ) : <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00</p>}
     </footer>
   );
+}
+
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>We're open from {openHour}:00 A.M until {closeHour}:00 P.M. Come visit us or order online!</p>
+      <button className="btn">Order</button>
+    </div>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
