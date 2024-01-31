d03cffa3493545b28b65b65810f0f13d9a974e7f
Nick Neessen <nick.neessen@gmail.com>
2024-01-31 17:45:43 -0600

    React Fragments <></>

--- src/index.js
+++ src/index.js
@@ -72,12 +72,19 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creatice dishes to choose from. All from 
+            our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : <p>We're still working on our menu. Please come back later.</p>}
     </main>
   );
