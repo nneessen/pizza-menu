c17e381f7a0a79a841ce497831c443982e0ca97a
Nick Neessen <nick.neessen@gmail.com>
2024-01-31 08:43:27 -0600

    completed lesson 49 react course

--- src/index.js
+++ src/index.js
@@ -72,18 +72,21 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
-      )}
+      ) : <p>We're still working on our menu. Please come back later.</p>}
     </main>
   );
 }
 
 function Pizza(props) {
+
+  if(props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
@@ -98,21 +101,18 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 10;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
-  // if (hour >= openHour && hour <= closeHour) alert("We're Open!");
-  // else alert("Sorry we are close");
-
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online!</p>
           <button className="btn">Order</button>
         </div>
-      )}
+      ) : <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00</p>}
     </footer>
   );
 }
